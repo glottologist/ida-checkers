da9306ec33213a1700a64972d3b4b01e0ca20442
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-11-15 20:51:37 +0800

    Added query

--- checkers-minimal/module/autocli.go
+++ checkers-minimal/module/autocli.go
@@ -8,21 +8,33 @@ import (
 // AutoCLIOptions implements the autocli.HasAutoCLIConfig interface.
 func (am AppModule) AutoCLIOptions() *autocliv1.ModuleOptions {
 	return &autocliv1.ModuleOptions{
-		Query: nil,
-        Tx: &autocliv1.ServiceCommandDescriptor{
-             Service: checkersv1.Msg_ServiceDesc.ServiceName,
-             RpcCommandOptions: []*autocliv1.RpcCommandOptions{
-                 {
-                     RpcMethod: "CreateGame",
-                     Use:       "create index black red",
-                     Short:     "Creates a new checkers game at the index for the black and red players",
-                     PositionalArgs: []*autocliv1.PositionalArgDescriptor{
-                         {ProtoField: "index"},
-                         {ProtoField: "black"},
-                         {ProtoField: "red"},
-                     },
-                 },
-             },
-         },
+        Query: &autocliv1.ServiceCommandDescriptor{
+            Service: checkersv1.Query_ServiceDesc.ServiceName,
+            RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+                {
+                    RpcMethod: "GetGame",
+                    Use:       "get-game index",
+                    Short:     "Get the current value of the game at index",
+                    PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+                        {ProtoField: "index"},
+                    },
+                },
+            },
+        },
+		Tx: &autocliv1.ServiceCommandDescriptor{
+			Service: checkersv1.Msg_ServiceDesc.ServiceName,
+			RpcCommandOptions: []*autocliv1.RpcCommandOptions{
+				{
+					RpcMethod: "CreateGame",
+					Use:       "create index black red",
+					Short:     "Creates a new checkers game at the index for the black and red players",
+					PositionalArgs: []*autocliv1.PositionalArgDescriptor{
+						{ProtoField: "index"},
+						{ProtoField: "black"},
+						{ProtoField: "red"},
+					},
+				},
+			},
+		},
 	}
 }
