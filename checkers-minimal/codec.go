5a8f14086f01300eb50916ee43c7cbcc79c74116
Jason Ridgway-Taylor <jason@glottologist.co.uk>
2023-11-15 20:07:28 +0800

    Added query.proto

--- checkers-minimal/codec.go
+++ checkers-minimal/codec.go
@@ -1,10 +1,16 @@
-package checkers
+    package checkers
 
-import (
-    types "github.com/cosmos/cosmos-sdk/codec/types"
-)
+    import (
+      types "github.com/cosmos/cosmos-sdk/codec/types"
+      sdk "github.com/cosmos/cosmos-sdk/types"
+      "github.com/cosmos/cosmos-sdk/types/msgservice"
+    )
 
-// RegisterInterfaces registers the interfaces types with the interface registry.
-func RegisterInterfaces(registry types.InterfaceRegistry) {
-}
+    // RegisterInterfaces registers the interfaces types with the interface registry.
+    func RegisterInterfaces(registry types.InterfaceRegistry) {
+      registry.RegisterImplementations((*sdk.Msg)(nil),
+          &MsgCreateGame{},
+      )
+      msgservice.RegisterMsgServiceDesc(registry, &_Msg_serviceDesc)
+    }
 
